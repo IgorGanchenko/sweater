a8e4e14e166033bfd08361086826aa219c3f054f
IgorGanchenko <navel86@mail.ru>
2022-07-16 09:17:22 +0300

    build(controllers, repo, dependency)
    added mustache
    spring security
    postgreSQL

---
+++ serving-web-content 2/src/main/java/com/example/servingwebcontent/controller/RegistrationController.java
@@ -0,0 +1,41 @@
+package com.example.servingwebcontent.controller;
+
+import com.example.servingwebcontent.domain.Role;
+import com.example.servingwebcontent.domain.User;
+import com.example.servingwebcontent.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/registration")
+public class RegistrationController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            model.put("message", "User exists!");
+            return "registration";
+        }
+
+        user.setActivity(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+
+        return "redirect:/login";
+    }
+}
