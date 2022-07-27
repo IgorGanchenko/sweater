0b63e7ea4f8110cd86b5b804e3ef024697be6c89
IgorGanchenko <navel86@mail.ru>
2022-07-27 14:28:53 +0300

    build(mail verification)

--- serving-web-content 2/src/main/java/com/example/servingwebcontent/controller/RegistrationController.java
+++ serving-web-content 2/src/main/java/com/example/servingwebcontent/controller/RegistrationController.java
@@ -1,41 +1,45 @@
 package com.example.servingwebcontent.controller;
 
-import com.example.servingwebcontent.domain.Role;
 import com.example.servingwebcontent.domain.User;
-import com.example.servingwebcontent.repos.UserRepo;
+import com.example.servingwebcontent.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/registration")
 public class RegistrationController {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
-    @GetMapping
+    @GetMapping("/registration")
     public String registration() {
         return "registration";
     }
 
-    @PostMapping
+    @PostMapping("/registration")
     public String addUser(User user, Map<String, Object> model) {
-        User userFromDb = userRepo.findByUsername(user.getUsername());
 
-        if (userFromDb != null) {
+        userService.addUser(user);
+        if (!userService.addUser(user)) {
             model.put("message", "User exists!");
             return "registration";
         }
-
-        user.setActivity(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepo.save(user);
-
         return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code) {
+        boolean isActivated = userService.activateUser(code);
+        if (isActivated) {
+            model.addAttribute("message", "User successfully activated");
+        } else {
+            model.addAttribute("message", "Activation code is not found!");
+        }
+        return "login";
     }
 }
--- serving-web-content 2/src/main/java/com/example/servingwebcontent/domain/User.java
+++ serving-web-content 2/src/main/java/com/example/servingwebcontent/domain/User.java
@@ -22,6 +22,9 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    private String email;
+    private String activationCode;
+
     public boolean isAdmin(){
         return roles.contains(Role.ADMIN);
     }
@@ -91,5 +94,21 @@ public class User implements UserDetails {
 
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getActivationCode() {
+        return activationCode;
+    }
+
+    public void setActivationCode(String activationCode) {
+        this.activationCode = activationCode;
     }
 }
